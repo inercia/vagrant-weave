5eb8d25e8426d924c8346c8769e38e88eda3d6f1
Alvaro Saurin <alvaro.saurin@gmail.com>
2014-12-07 00:22:38 +0100

    Improved configuration and provisioning.

--- Vagrantfile
+++ Vagrantfile
@@ -1,47 +1,62 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# some recommended plugins:
-# vagrant plugin install vagrant-hostsupdater
-
-# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 
-Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  # All Vagrant configuration is done here. The most common configuration
-  # options are documented and commented below. For a complete reference,
-  # please see the online documentation at vagrantup.com.
+# used for exporting the Weave sources
+GOPATH=ENV['GOPATH']
 
-  # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "robwc/minitrusty64"    # a minimal Ubuntu
+# name, external IP and internal bridge addresses
+CFG = [ ["weave1", "192.168.40.11", "172.17.51.1"],
+        ["weave2", "192.168.40.12", "172.17.52.1"] ]
+
+###########################################################################
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # config.vm.box = "robwc/minitrusty64"    # a minimal Ubuntu
+  config.vm.box = "baremettle/ubuntu-14.04"
 
   config.vm.box_check_update = false
-  config.vm.network :forwarded_port, guest: 2375, host: 2375, auto_correct: true
+
+  ["virtualbox", "vmware_fusion", "vmware_workstation", "libvirt"].each do |p|
+    config.vm.provider "p" do |v|
+      v.vmx["memsize"]              = "1024"
+      v.vmx["numvcpus"]             = "1"
+      v.vmx["cpuid.coresPerSocket"] = "1"
+    end
+  end
+
+  config.vm.provider :libvirt do |libvirt|
+    libvirt.driver = "kvm"
+    libvirt.storage_pool_name = "default"
+  end
+
+  #########################
+  # shared folders
+  #########################
 
   # see this page for running NFS without asking password
   # http://joemaller.com/3411/vagrant-nfs-shares-without-a-password/
   config.vm.synced_folder ".", "/vagrant", create: true, type: "nfs"
 
-  ["virtualbox", "vmware_fusion", "vmware_workstation"].each do |p|
-    config.vm.provider "p" do |v|
-      v.vmx["memsize"] = "2048"
-      v.vmx["numvcpus"] = "2"
-      v.vmx["cpuid.coresPerSocket"] = "1"
-    end
-  end
+  # export the weave:export directory
+  config.vm.synced_folder "/var/tmp", "/weave/images", create: true, type: "nfs"
 
-  # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.provision "shell", path: "provision.d/all.sh"
+  # export the weave sources
+  config.vm.synced_folder GOPATH, "/weave/src", create: true, type: "nfs"
 
-  config.vm.define "vm1" do |vm1|
-    vm1.vm.hostname = "vm1"
-    vm1.vm.network "private_network", ip: "192.168.40.11"
-    vm1.vm.provision "shell", path: "provision.d/vm1.sh"
-  end
+  #########################
+  # provisioning
+  #########################
 
-  config.vm.define "vm2" do |vm2|
-    vm2.vm.hostname = "vm2"
-    vm2.vm.network "private_network", ip: "192.168.40.12"
-    vm2.vm.provision "shell", path: "provision.d/vm2.sh"
+  CFG.each do |name, ext_ip, bridge_ip|
+	  config.vm.define name do |weave|
+	    weave.vm.hostname = name
+	    weave.vm.network "private_network", ip:ext_ip
+	    weave.vm.provision "shell",
+	    	path: "provision.d/all.sh",
+	    	args: "#{ext_ip} #{bridge_ip}"
+	  end
   end
 end
+
--- provision.d/all.sh
+++ provision.d/all.sh
@@ -4,7 +4,7 @@
 WEAVE_EXE=/usr/local/bin/weave
 
 # where do we get Weave from
-WEAVE_ORIGIN_URL=https://raw.githubusercontent.com/zettio/weave/master/weave
+WEAVE_ORIGIN_URL=/weave/src/src/github.com/zettio/weave/weave
 
 # basic debs to install
 DEBS_BASIC="apparmor \
@@ -17,6 +17,28 @@ golang-go.tools \
 golang-go.tools-dev \
 conntrack \
 ethtool"
+
+# external, host-accessible IP
+EXT_IP=$1
+
+# Docker port
+PORT=2375
+
+# every container launched on this VM will get an IP address 
+# from range 172.17.51.2 – 172.17.51.255
+IP=$2
+MASK=255.255.255.0
+
+# the bridge device
+BRIDGE=bridge0
+
+# certificates (if using TLS)
+CA=/etc/docker/certs/ca.pem
+CERT=/etc/docker/certs/cert.pem
+KEY=/etc/docker/certs/key.pem
+
+# Docker daemon defaults file
+DEFAULTS=/etc/default/docker
 
 ###################################################################################
 
@@ -41,7 +63,12 @@ if [ -d /vagrant/certs ] ; then
   cp -R /vagrant/certs /etc/docker/certs
 fi
 
-sudo apt-get update
+[ ! -f /var/cache/apt/pkgcache.bin ] || \
+    /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null
+if [ $? -eq 0 ] ; then
+  echo "[provision] Needs to apt-get update"
+  sudo apt-get update
+fi
 
 # Check that HTTPS transport is available to APT
 if [ ! -e /usr/lib/apt/methods/https ]; then
@@ -52,7 +79,7 @@ fi
 
 sudo apt-get install -y $DEBS_BASIC
 
-if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
+if [ ! -f "/etc/apt/sources.list.d/docker.list" ] ; then
   echo "[provision] Adding the repository to your APT sources..."
   echo "deb https://get.docker.com/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
 
@@ -67,10 +94,57 @@ sudo apt-get install -y lxc-docker
 # echo "[provision] Getting Ubuntu base..."
 # sudo docker pull ubuntu
 
-echo "[provision] Installing Weave..."
-if [[ "$(sudo curl $WEAVE_ORIGIN_URL -z $WEAVE_EXE -o $WEAVE_EXE -s -L -w %{http_code})" == "200" ]]; then
-  echo "[provision] Weave has been updated"
-  sudo chmod a+x $WEAVE_EXE
+echo "[provision] Installing Weave script..."
+sudo cp -f $WEAVE_ORIGIN_URL $WEAVE_EXE
+sudo chmod a+x $WEAVE_EXE
+
+################################################
+
+brctl show | grep $BRIDGE >/dev/null 2>/dev/null
+if [ $? -eq 1 ] ; then
+  echo "[provision] Setting up $BRIDGE"
+  sudo brctl addbr $BRIDGE
+fi
+sudo ifconfig $BRIDGE $IP netmask $MASK
+
+grep "DOCKER_OPTS" "$DEFAULTS" | grep "$BRIDGE" >/dev/null 2>/dev/null
+if [ $? -eq 1 ] ; then
+  echo "[provision] Setting Docker daemon flags..."
+  echo "DOCKER_OPTS=\"-b=$BRIDGE -H tcp://0.0.0.0:$PORT -H unix:///var/run/docker.sock --tlsverify=false\"" >> $DEFAULTS
 fi
 
+echo "[provision] Restarting Docker"
+sudo service docker restart
+sleep 1
 
+ORPHANS=`sudo docker images | grep \<none\> | awk '{ print $3 }'` 
+for IMAGE in $ORPHANS ; do
+  echo "[provision] Removing image $IMAGE..."
+  sudo docker rmi $IMAGE
+done
+
+if [ -f /weave/images/weave.tar ] ; then
+  echo "[provision] Removing old Weave image..."
+  sudo docker rm --force weave 2>/dev/null
+  sudo docker images zettio/weave | grep zettio/weave && sudo docker rmi -f zettio/weave
+
+  echo "[provision] Importing Weave image..."
+  sudo docker load --input /weave/images/weave.tar
+fi
+
+if [ -f /weave/images/weavedns.tar ] ; then
+  echo "[provision] Removing old Weave-DNS image..."
+  sudo docker rm --force weavedns 2>/dev/null
+  sudo docker images zettio/weavedns | grep zettio/weavedns && sudo docker rmi -f zettio/weavedns
+
+  echo "[provision] Importing Weave-DNS image..."
+  sudo docker load --input /weave/images/weavedns.tar
+fi
+
+echo "[provision]"
+echo "[provision] ------------------------------------------------"
+echo "[provision] Check the Docker in this machine is usable with:"
+echo "[provision]"
+echo "[provision] $ DOCKER_HOST=tcp://$EXT_IP:$PORT docker info"
+echo "[provision]"
+echo "[provision] ------------------------------------------------"
